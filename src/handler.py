749faa84e345fddf99db4dba24dd9f2bd91452ce
Matheus Ribeiro <teteuoliveira12@gmail.com>
2025-10-05 02:28:13 -0300

    feat(create-lambda-resources): get_auth_use_case

--- src/handler.py
+++ src/handler.py
@@ -7,7 +7,6 @@ from src.aws_cognito_idp import AuthService
 from src.aws_secrets_manager import get_aws_secrets
 from src.get_auth_use_case import get_auth_use_case
 from src.http_response import http_response
-from src.post_auth_ser_case import post_auth_use_case
 
 
 def lambda_handler(event, context):
@@ -21,13 +20,13 @@ def lambda_handler(event, context):
 
     method = event.get("requestContext", {}).get("http", {}).get("method")
 
-    # if method == "OPTIONS":
-    #     return http_response(200, {"message": "CORS preflight check successful"})
-    #
-    # if method == "GET":
-    #     query = event.get("queryStringParameters", {})
-    #     return get_auth_use_case(service=service, query=query)
-    #
+    if method == "OPTIONS":
+        return http_response(200, {"message": "CORS preflight check successful"})
+
+    if method == "GET":
+        query = event.get("queryStringParameters", {})
+        return get_auth_use_case(service=service, query=query)
+
     # if method == "POST":
     #     body = json.loads(event.get("body", "{}"))
     #     return post_auth_use_case(service=service, data=body)
